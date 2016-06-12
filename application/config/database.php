315a71e7b30b0dff7afd03ff4b4a2b875acd2462
sun zhuo <sunzhuo1228@126.com>
2016-06-13 00:32:04 +0800

    close mysql

--- application/config/database.php
+++ application/config/database.php
@@ -70,27 +70,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
-$query_builder = TRUE;
+#$active_group = 'default';
+#$query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'mysql',
-	'username' => 'root',
-	'password' => '123456',
-	'database' => 'kindle_assistant',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+#$db['default'] = array(
+#	'dsn'	=> '',
+#	'hostname' => 'mysql',
+#	'username' => 'root',
+#	'password' => '123456',
+#	'database' => 'kindle_assistant',
+#	'dbdriver' => 'mysqli',
+#	'dbprefix' => '',
+#	'pconnect' => FALSE,
+#	'db_debug' => (ENVIRONMENT !== 'production'),
+#	'cache_on' => FALSE,
+#	'cachedir' => '',
+#	'char_set' => 'utf8',
+#	'dbcollat' => 'utf8_general_ci',
+#	'swap_pre' => '',
+#	'encrypt' => FALSE,
+#	'compress' => FALSE,
+#	'stricton' => FALSE,
+#	'failover' => array(),
+#	'save_queries' => TRUE
+#);
+#
