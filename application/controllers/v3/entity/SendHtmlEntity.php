61b4af6b55ac68f445dd7e310c0737089e63c8e5
sun zhuo <sunzhuo1228@126.com>
2016-06-16 23:53:11 +0800

    test temp file logic

--- application/controllers/v3/entity/SendHtmlEntity.php
+++ application/controllers/v3/entity/SendHtmlEntity.php
@@ -96,6 +96,8 @@ class SendHtmlEntity
 
     public function saveHtml2Local()
     {
+        /**
+
         $htmlPath = '/tmp/html_record/';
         if (!is_dir($htmlPath)) {
             mkdir($htmlPath, 0744);
@@ -134,10 +136,11 @@ class SendHtmlEntity
 
         // 正则替换图片
         $html = preg_replace_callback($pattern_src, "self::changeImgLocal", $this->toHtml());
-
+         *
+         */
         $tmpHandle = tmpfile();
         $htmlPath = stream_get_meta_data($tmpHandle)['uri'];
-        @fwrite($tmpHandle, $html);
+        @fwrite($tmpHandle, $this->toHtml());
 
         return $htmlPath;
     }
